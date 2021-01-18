156fa63d487eb2947810d97b51b20dc3cd393363
wei yuan <737571078@qq.com>
2021-01-18 11:18:31 +0800

    添加项目文件。

---
+++ test/expensereportpage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// expensereportpage.xaml 的交互逻辑
+    /// </summary>
+    public partial class expensereportpage : Page
+    {
+        public expensereportpage()
+        {
+            InitializeComponent();
+        }
+
+        public expensereportpage(object data):this()
+        {
+            this.DataContext = data;
+        }
+    }
+}
